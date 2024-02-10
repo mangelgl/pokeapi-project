26fe215a963088b17982814238bba2e4a3640fa7
Miguel Ángel García <magarcial.dev@outlook.es>
2024-02-10 12:15:22 +0100

    Added getPokemonTypesById function Part 2

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,24 @@
 import { pokemonOptions, getPokemonNameById, getPokemonTypesById } from "./controllers/pokemonController";
 
-const pokemonList: pokemonOptions[] = [];
 
-const promises = [];
+async function processPokemon(number: number) {
 
-/**
- * TODO: Averiguar por qué no funciona, se ejecuta pero no muestra la lista {id: index, name: pokemon, type: types}
- */
-for (let index = 1; index <= 3; index++) {
+    const pokemonList: pokemonOptions[] = [];
     
-    const promise = getPokemonNameById(index)
-        .then( (pokemon: string) => {
-            getPokemonTypesById(index)
-                .then((types: string[]) => {                        
-                    pokemonList.push({id: index, name: pokemon, type: types});
-                })
-                .catch( (err) => console.error(`An error has ocurred: ${err}`) );
-                
-        })
-        .catch( (err) => console.error(`An error has ocurred: ${err}`) );
+    for (let index = 1; index <= number; index++) {
+        
+        try {
+            
+            const name = await getPokemonNameById(index);
+            const type = await getPokemonTypesById(index);
+    
+            pokemonList.push({id: index, name: name, type: type});        
+        } catch (error) {
+            console.error(`An error has ocurred: ${error}`);
+        }
+    }
 
-    promises.push(promise);
+    return pokemonList;
 }
 
-Promise.all(promises)
-    .then( () => {
-        pokemonList.sort( (a, b) => a.id - b.id);
-        pokemonList.forEach( (pokemon) => console.log({pokemon}) );
-    })
-    .catch( (err) => console.error(`An error has ocurred: ${err}`) );
+processPokemon(9).then( (pokemon) => console.log(pokemon) );
